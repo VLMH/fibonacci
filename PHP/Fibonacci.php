b43eba5cfcbb34cd16c7ffbe9ba4f644020e17f1
Victor Lam <victorebox@yahoo.com.hk>
2015-05-20 21:42:23 +0800

    PHP implementation

---
+++ PHP/Fibonacci.php
@@ -0,0 +1,37 @@
+<?php
+
+class Fibonacci
+{
+    public function run($number = 10, $method = 'r')
+    {
+        switch ($method) {
+            case 'recursion':
+            case 'r':
+                return $this->fib_recursion($number);
+            case 'dynamic':
+            case 'd':
+                return $this->fib_dynamic($number);
+        }
+    }
+
+    private function fib_recursion($n)
+    {
+        if ($n <= 1)
+            return $n;
+        return $this->fib_recursion($n - 1) + $this->fib_recursion($n - 2);
+    }
+
+    private function fib_dynamic($n)
+    {
+        $values = [0, 1];
+
+        for ($i = 2; $i <= $n; $i++) {
+            $values[$i] = $values[$i - 1] + $values[$i - 2];
+        }
+
+        return array_pop($values);
+    }
+}
+
+$result = (new Fibonacci())->run(30, 'r');
+print("fib(10) => $result\n");
