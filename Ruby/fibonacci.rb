41c2f5b6e0b2027820b37e93d5c8925c870c9daf
Victor Lam <victorebox@yahoo.com.hk>
2015-05-27 20:05:11 +0800

    revise Ruby implementation to make it dynamic for CLI

--- Ruby/fibonacci.rb
+++ Ruby/fibonacci.rb
@@ -27,5 +27,19 @@ class Fibonacci
     end
 end
 
-n = 30
-puts "fib(#{n}) => #{Fibonacci.new.run n, 'd'}"
+# default value
+n = 10
+method = 'd'
+
+i = 0
+while i < ARGV.count
+    case ARGV[i]
+    when '--method', '-m'
+        method = ARGV[i+=1]
+    else
+        n = ARGV[i].to_i
+    end
+    i += 1
+end
+
+puts "fib(#{n}) => #{Fibonacci.new.run n, method}"
