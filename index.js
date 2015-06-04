13fb87051118a6435ee855bea5c0ce997f86fc21
Victor Lam <victorebox@yahoo.com.hk>
2015-06-04 13:44:29 +0800

    communicate with websocket to get fibonacci result

---
+++ index.js
@@ -0,0 +1,76 @@
+var WebSocketServer = require('ws').Server
+var http = require("http")
+var express = require("express")
+var app = express()
+var port = 5000
+var exec = require('child_process').exec
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/fibonacci/:fib', function(req, res) {
+    var fib = req.params.fib
+    var by = req.query.by
+
+    var start = Date.now()
+    exec('ruby ' + __dirname + '/Ruby/main.rb -m ' + by + ' ' + fib, function(err, stdout, stderr) {
+        res.send(
+            stdout + '<br>'
+            + 'Time used: ' + (Date.now() - start) + 'ms'
+        )
+    })
+})
+
+app.get('/fibonacci', function(req, res) {
+    res.sendFile(__dirname + '/fibonacci.html')
+})
+
+app.post('/fibonacci', function(req, res) {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+var server = http.createServer(app)
+server.listen(port)
+
+console.log("http server listening on %d", port)
+
+var wss = new WebSocketServer({server: server})
+console.log("websocket server connected")
+
+wss.on("connection", function(ws) {
+    // var id = setInterval(function() {
+    //     ws.send(JSON.stringify(new Date()), function() {})
+    // }, 1000)
+
+    console.log("websocket connection open")
+
+    ws.on("open", function open() {
+        console.log("connected")
+    })
+
+    ws.on("message", function message(data, flags) {
+        console.log(data, flags)
+
+        var params = JSON.parse(data)
+        var cmd = 'ruby ' + __dirname + '/Ruby/main.rb -m ' + params.method + ' ' + params.number.toString(10)
+        var start = Date.now()
+
+        exec(cmd, function(err, stdout, stderr) {
+            var output = {
+                result: stdout.trim(),
+                time_spent: Date.now() - start,
+            }
+            ws.send(JSON.stringify(output), function() {})
+        })
+    })
+
+    ws.on("close", function() {
+        console.log("websocket connection close")
+        // clearInterval(id)
+    })
+})
